b8d5fe4f96425b4151a776eec9da2e1e9e6e9b4e
AlomariKa <omarikhaled1999@gmail.com>
2025-03-04 05:36:08 -0500

    Update Auth

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Hotel, Room, Employee
 # Register your models here.
+
+admin.site.register(Hotel)
+admin.site.register(Room)
+admin.site.register(Employee)
